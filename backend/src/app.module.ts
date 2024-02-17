aaa8a59ec3ae914603b64a0c3fabb75fafab6691
sanjueranga <sanjueranga16@gmail.com>
2024-02-17 11:09:24 +0530

    database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import dbConfig from './config/db/orm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(dbConfig()), // Fix this line
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
